c0f24ff5f0a87a273b5689f96e5f5687c6843524
Vivek Gandhi <gandhiv@raiju.uwb.edu>
2021-05-02 06:46:05 -0700

    minor changes

--- Driver.m
+++ Driver.m
@@ -5,6 +5,8 @@ clc
 
 outputFileName = 'test-small-out-911.xml';
 edgesFileName = 'allEdges.xml';
+jpgFileName = 'output.jpg';
+
 ouputSt = parseOutput(outputFileName, edgesFileName);
 
 eList = ouputSt.newEdges;
@@ -13,5 +15,5 @@ vList = ouputSt.vertexTypesPostEvent;
 % eList = ouputSt.oldEdges;
 % vList = ouputSt.vertexTypesPreEvent;
 
-makeMap(eList, vList, ouputSt)
+makeMap(eList, vList, ouputSt, jpgFileName)
 
